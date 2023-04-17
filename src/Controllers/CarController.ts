6f5363bb216de15c50f2edf0abcbfa719766022d
Alissa Maximo <alissahiratamaximo@gmail.com>
2023-04-17 18:33:27 -0300

    CarController ajustado

--- src/Controllers/CarController.ts
+++ src/Controllers/CarController.ts
@@ -1,6 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
 import CarService from '../Services/CarService';
-import ICar from '../Interfaces/ICar';
 
 export default class CarController {
   private service: CarService;
@@ -10,17 +9,8 @@ export default class CarController {
   }
 
   public async createCar(req: Request, res: Response, next: NextFunction) {
-    const car: ICar = {
-      model: req.body.model,
-      year: req.body.year,
-      color: req.body.color,
-      buyValue: req.body.buyValue,
-      status: req.body.status,
-      doorsQty: req.body.doorsQty,
-      seatsQty: req.body.seatsQty,
-    };
     try {
-      const newCar = await this.service.createCar(car);
+      const newCar = await this.service.createCar(req.body);
       return res.status(201).json(newCar);
     } catch (error) {
       next(error);
