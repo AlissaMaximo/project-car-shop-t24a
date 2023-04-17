025a57d530040a8417098ae1ccb868964c9f9d02
Alissa Maximo <alissahiratamaximo@gmail.com>
2023-04-17 19:58:00 -0300

    app.use criado em app.ts

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import AutomobileRouter from './Routes/AutomobileRoutes';
 
 const app = express();
+
+app.use(express.json());
 app.use(AutomobileRouter);
 
 export default app;
